9ef201c5ebe1fb9a34f7fabd5c736d42eeb005a7
Kayphrosh <olakunbiolabode@yahoo.com>
2024-10-19 14:43:04 +0100

    fix bug

--- src/pages/livebet/[id].tsx
+++ src/pages/livebet/[id].tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import BetOverview from '@/components/dashboard/livebet-overview';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
-// Add this function at the top of your file
-const safeBigInt = (value: string | number | bigint | undefined) => {
-  if (value === undefined) return BigInt(0);
-  return BigInt(value);
-};
+
 
 const index = () => {
 
